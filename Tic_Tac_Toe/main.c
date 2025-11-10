127e9fc334b2c4e3208559f636a5df08de26c919
yamato-spirit <mamo831sk@gmail.com>
2025-11-10 21:04:17 +0900

    関数の大枠定義

---
+++ Tic_Tac_Toe/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// 1.盤面初期化(1回のみ)
+void init_board(void) {
+	return;
+ }
+// 2.盤面表示
+void display_board(void) {
+	return;
+}
+// 3.ユーザーの入力受付
+void get_user_input(void) {
+	return;
+}
+// 4.入力の不正確認
+void place_mark(void) {
+	return;
+}
+// 5.勝敗判定
+void check_win(void) {
+	return;
+}
+// 6.引き分け判定
+void check_draw(void) {
+	return;
+}
+// 7.プレイヤー交代
