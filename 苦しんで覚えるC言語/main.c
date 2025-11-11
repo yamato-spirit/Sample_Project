bfa6a3672c3842d3b234c3758f2a8971ac9fcb08
yamato-spirit <mamo831sk@gmail.com>
2025-11-11 22:04:46 +0900

    ～課題5章

--- 苦しんで覚えるC言語/main.c
+++ 苦しんで覚えるC言語/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("40÷13について、商は%d, 余りは%dです\n", 40 / 13, 40 % 13);
+	int drink = 198;
+	int milk = 138;
+	float tax = 0.05; // 消費税率
+	int sum, change;
+	sum = drink * 1 + milk * 2; // 金額合計(税抜き)
+	change = 1000 - (int)(sum * (1 + tax)); // おつり (int) → キャスト変換を忘れない！！
+	printf("清涼飲料水1本と牛乳2本を1,000円で購入した際のおつりは%d円です\n", change);
 	return 0;
 }
